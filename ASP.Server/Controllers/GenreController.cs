275c4df945fd660193980647f291f4f3283ee7c0
YoussefNeffati <youssef.neffati06@gmail.com>
2023-02-16 11:17:21 +0100

    add a genre

--- ASP.Server/Controllers/GenreController.cs
+++ ASP.Server/Controllers/GenreController.cs
@@ -6,9 +6,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace ASP.Server.Controllers
 {
+    public class CreateGenreModel
+    {
+        [Required(ErrorMessage = "Please enter the genre name")]
+        [Display(Name = "GenreLitteraire")]
+        public String Name { get; set; }
+    }
+
     public class GenreController : Controller
     {
         private readonly LibraryDbContext libraryDbContext;
@@ -22,6 +31,26 @@ namespace ASP.Server.Controllers
         {
             List<Genre> genres = libraryDbContext.Genres.Include(b => b.Books).ToList();
             return View(genres);
+        }
+
+        public ActionResult<CreateGenreModel> Create(CreateGenreModel genre)
+        {
+            if (ModelState.IsValid)
+            {
+                Genre newGenre = new Genre
+                {
+                    GenreLitteraire = genre.Name
+                };
+
+                libraryDbContext.Genres.Add(newGenre);
+                libraryDbContext.SaveChanges();
+
+                return RedirectToAction(nameof(List));
+            }
+
+            return View(genre);
+
+
         }
     }
 }

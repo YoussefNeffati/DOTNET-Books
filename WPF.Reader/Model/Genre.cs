8ce3a1b02c26c40ad9dd53435176b555e3966d16
YoussefNeffati <youssef.neffati06@gmail.com>
2023-02-16 17:44:00 +0100

    bugs

---
+++ WPF.Reader/Model/Genre.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WPF.Reader.Model
+{
+   
+
+        // Mettez ici les propriété de votre livre: Nom et Livres associés
+
+        // N'oublier pas qu'un genre peut avoir plusieur livres
+    
+    public enum GenresPossible
+    {
+        Roman,
+        Nouvelle,
+        Poesie,
+        Theatre,
+        Essai,
+        Biographie,
+        Autobiographie,
+        Memoire,
+        ScienceFiction,
+        Fantastique,
+        Policier,
+        Horreur,
+        Romance,
+        Aventure,
+        Historique,
+        Epistolaire,
+        Humoristique,
+        Erotique,
+        Jeunesse,
+        TheorieLitteraire
+    }
+}
--- WPF.Reader/Service/LibraryService.cs
+++ WPF.Reader/Service/LibraryService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using WPF.Reader.Model;
 
 namespace WPF.Reader.Service
@@ -10,11 +12,51 @@ namespace WPF.Reader.Service
         // Donc pas de LibraryService.Instance.Books = ...
         // mais plutot LibraryService.Instance.Books.Add(...)
         // ou LibraryService.Instance.Books.Clear()
-        public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>() {
-            new Book(),
-            new Book(),
-            new Book()
+
+
+        public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>()
+        {
+
+
+
+
         };
+
+        public LibraryService()
+        {
+            List<Book> books = new List<Book>();
+
+            // Créer un premier livre
+            Book livre1 = new Book()
+            {
+                Id = 1,
+                Nom = "Le Seigneur des anneaux",
+                Prix = 20,
+                Auteur = "J.R.R. Tolkien",
+                Contenu = "Dans un monde imaginaire appelé la Terre du Milieu, un hobbit nommé Frodon Sacquet doit détruire un anneau magique avant qu'il ne tombe entre les mains du mal.",
+                Genres = new List<Genre>(){
+                    new Genre(){ Id = 1, GenreLitteraire = "Fantasy" },
+                       new Genre(){ Id = 2, GenreLitteraire = "Aventure" },
+                    }
+            };
+            books.Add(livre1);
+
+            // Créer un deuxième livre
+            Book livre2 = new Book()
+            {
+                Id = 2,
+                Nom = "Fondation",
+                Prix = 18,
+                Auteur = "Isaac Asimov",
+                Contenu = "Dans un futur lointain, un scientifique créé une fondation destinée à préserver les connaissances de l'humanité face à l'effondrement de la civilisation.",
+                Genres = new List<Genre>(){
+                     new Genre(){ Id = 3, GenreLitteraire = "Science-fiction" },
+                
+            }
+            };
+            books.Add(livre2);
+
+        }
 
         // C'est aussi ici que vous ajouterez les requête réseau pour récupérer les livres depuis le web service que vous avez fait
         // Vous pourrez alors ajouter les livres obtenu a la variable Books !
